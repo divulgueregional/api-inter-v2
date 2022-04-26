e351fdc7d9a157d50e82b888d4b6f5ac856495bc
divulgueregional <roseno@divulgueregional.com.br>
2022-04-26 20:09:22 -0300

    consultar extrato

--- src/InterBanking.php
+++ src/InterBanking.php
@@ -110,7 +110,39 @@ class InterBanking
             try {
                 $response = $this->client->request(
                     'GET',
-                    '/banking/v2/saldo?dataSaldo='.$this->dd->dataSaldo,
+                    "/banking/v2/saldo?dataSaldo={$this->dd->dataSaldo}",
+                    [
+                        'verify' => $this->dd->certificate,
+                        'cert' => $this->dd->certificate,
+                        'ssl_key' => $this->dd->certificateKey,
+                        'headers' => [
+                            'Accept' => 'application/json',
+                            'Authorization' => "Bearer {$this->token}"
+                        ]
+                    ]
+                );
+
+                return json_decode($response->getBody()->getContents());
+            } catch (ClientException $e) {
+                $statusCode = $e->getResponse()->getStatusCode();
+                $response = $e->getResponse()->getReasonPhrase();
+
+                return ['error' => $response, 'statusCode' => $statusCode];
+            } catch (\Exception $e) {
+                throw new Exception("Falha ao consultar saldo: {$e->getMessage()}");
+            }
+        }else{
+            return $this->ok;
+        }
+    }
+
+    public function checkExtrato()
+    {
+        if($this->ok==''){
+            try {
+                $response = $this->client->request(
+                    'GET',
+                    "/banking/v2/extrato?dataInicio={$this->dd->dataInicio}&dataFim={$this->dd->dataFim}",
                     [
                         'verify' => $this->dd->certificate,
                         'cert' => $this->dd->certificate,
